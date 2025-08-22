e05305d3d3c204cbb203bbc1cf44a47555aa25a5
Mark Sobkow <mark.sobkow@gmail.com>
2025-08-22 05:58:24 -0600

    Latest

--- src/main/java/org/msscf/msscf/v2_13/cfsec/CFSecRam/CFSecRamSecFormTable.java
+++ src/main/java/org/msscf/msscf/v2_13/cfsec/CFSecRam/CFSecRamSecFormTable.java
@@ -8,7 +8,7 @@
  *	
  *	MSS Code Factory CFSec 2.13 Security Essentials
  *	
- *	Copyright 2020-2021 Mark Stephen Sobkow mark.sobkow@gmail.com
+ *	Copyright 2020 Mark Stephen Sobkow mark.sobkow@gmail.com
  *	
  *	Licensed under the Apache License, Version 2.0 (the "License");
  *	you may not use this file except in compliance with the License.
@@ -33,6 +33,7 @@ import java.text.*;
 import java.util.*;
 import org.apache.commons.codec.binary.Base64;
 import org.msscf.msscf.v2_13.cflib.CFLib.*;
+import org.msscf.msscf.v2_13.cflib.CFLib.xml.*;
 import org.msscf.msscf.v2_13.cfsec.CFSec.*;
 import org.msscf.msscf.v2_13.cfsec.CFSecObj.*;
 import org.msscf.msscf.v2_13.cfsec.CFSecObj.*;
